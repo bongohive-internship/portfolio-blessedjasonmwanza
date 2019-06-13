9d16d5c07d1307611c4df1a9ff8c197cf5166eb9
blessedjasonmwanza <mwanzabj@gmail.com>
2019-06-14 00:47:14 +0200

    Added My Favorite Languages Section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import  './css/Contact.css';
+import './css/FavoriteLangues.css';
 import TopNav from './components/TopNav';
 import Contact from './components/contact';
+import FavoriteLangues from './components/favorite-languages';
 function App() {
   return (
     <div className="App">
+      <span className="margin" />
       < TopNav />
+      < FavoriteLangues />
       < Contact />
     </div>
   );
